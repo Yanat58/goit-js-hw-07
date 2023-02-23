8285abc554161f1b581987aa5db823e0b7223415
Tetiana Karlova <tanyalyt80@gmail.com>
2023-02-23 23:52:10 +0200

    fixed task-1.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -23,10 +23,10 @@ const createGalleryImages = galleryItems
 
 // console.log(createGalleryImages);
 divRefClassGallery.insertAdjacentHTML("beforeend", createGalleryImages);
-
 divRefClassGallery.addEventListener("click", onImageClick);
 
-function onImageClick(event) {
+function onImageOpen(event) {
+  console.log(event);
   event.preventDefault();
   if (!event.target.classList.contains("gallery__image")) {
     return;
@@ -36,12 +36,42 @@ function onImageClick(event) {
     <img src="${event.target.dataset.source}" width="800" height="600">
 `);
   instance.show();
+  console.log(event);
+  document.addEventListener("keydown", onImageClose);
 
-document.addEventListener("keydown", onImageClose);
-
-function onImageClose(event) {
-  if (event.code === "Escape") {
-    instance.close();
+  function onImageClose(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+    console.log(event.key);
+    console.log(event.code);
   }
 }
-}
+
+// const instance = basicLightbox.create(
+//   `
+// <img src="" width="800" height="600" >`,
+//   {
+//     onShow: (instance) => {
+//       document.addEventListener("keydown", onEscKeyPress);
+//     },
+//     onClose: (instance) => {
+//       window.removeEventListener("keydown", onEscKeyPress);
+//     },
+//   }
+// );
+
+// function onImageClick(event) {
+//   event.preventDefault();
+//   if (!event.target.dataset.source) {
+//     return;
+//   }
+//   instance.element().querySelector("img").src = dataset.source;
+//   instance.show();
+// }
+
+// function onEscKeyPress(event) {
+//   cconsole.log(event);
+//   if (event.code !== "Escape") return;
+//   instance.close();
+// }
