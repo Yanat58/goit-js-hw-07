e05b042540b1f765e336afd4ac24703e6df65bc4
Tetiana Karlova <tanyalyt80@gmail.com>
2023-02-23 15:35:02 +0200

    task-2.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
@@ -20,3 +20,16 @@ const createGalleryImages = galleryItems
 
 // console.log(createGalleryImages);
 listRefClassGallery.insertAdjacentHTML("beforeend", createGalleryImages);
+// let lightbox = new SimpleLightbox(".gallery a", {
+//   captions: true,
+//   captionSelector: "img",
+//   captionType: "attr",
+//   captionsData: "alt",
+//   captionPosition: "bottom",
+//   captionDelay: 250,
+// });
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
