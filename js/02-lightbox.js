9d2ba88b8c85c9a8590babe1f76345fb05ec32ed
Yanat58 <tanyalyt80@gmail.com>
2023-02-23 14:45:21 +0200

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listRefClassGallery = document.querySelector(".gallery");
+
+const createGalleryImages = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a> `
+  )
+  .join("");
+
+// console.log(createGalleryImages);
+listRefClassGallery.insertAdjacentHTML("beforeend", createGalleryImages);
