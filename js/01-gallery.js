7ad0d74649e6e90c169865ec06b06f1831d64e58
Tetiana Karlova <tanyalyt80@gmail.com>
2023-02-26 14:53:20 +0200

    fixed task-10

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,53 +25,23 @@ const createGalleryImages = galleryItems
 divRefClassGallery.insertAdjacentHTML("beforeend", createGalleryImages);
 divRefClassGallery.addEventListener("click", onImageClick);
 
-function onImageOpen(event) {
-  console.log(event);
+function onImageClick(event) {
   event.preventDefault();
   if (!event.target.classList.contains("gallery__image")) {
     return;
   }
-
   const instance = basicLightbox.create(`
     <img src="${event.target.dataset.source}" width="800" height="600">
 `);
   instance.show();
-  console.log(event);
-  document.addEventListener("keydown", onImageClose);
+  onClose: () => {
+    divRefClassGallery.removeEventListener("click", onImageClick);
+  },
+    document.addEventListener("keydown", onImageClose, { once: true });
 
   function onImageClose(event) {
     if (event.code === "Escape") {
       instance.close();
     }
-    console.log(event.key);
-    console.log(event.code);
   }
 }
-
-// const instance = basicLightbox.create(
-//   `
-// <img src="" width="800" height="600" >`,
-//   {
-//     onShow: (instance) => {
-//       document.addEventListener("keydown", onEscKeyPress);
-//     },
-//     onClose: (instance) => {
-//       window.removeEventListener("keydown", onEscKeyPress);
-//     },
-//   }
-// );
-
-// function onImageClick(event) {
-//   event.preventDefault();
-//   if (!event.target.dataset.source) {
-//     return;
-//   }
-//   instance.element().querySelector("img").src = dataset.source;
-//   instance.show();
-// }
-
-// function onEscKeyPress(event) {
-//   cconsole.log(event);
-//   if (event.code !== "Escape") return;
-//   instance.close();
-// }
